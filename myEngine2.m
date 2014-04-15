db6f29cfc521b6ef012427e6399ba2f936658784
magicpiratex <magicpiratex@gmail.com>
2014-04-15 21:08:11 +0800

    More data on exp3

--- myEngine2.m
+++ myEngine2.m
@@ -1,9 +1,9 @@
 clc;
 close all;
 fid = 10.1;
-for w = 1:0.4:5
+for w = 0.1:0.4:5
     
-    fprintf('----- %d -------\n', fid)
+    fprintf('----- %f: %f -------\n', fid, w)
     main2(5000, 5, 15, w, 1.0, fid);
     
     fid = fid + 0.1;
--- myEngine3.m
+++ myEngine3.m
@@ -5,8 +5,8 @@ close all;
 fid = 11.1; 
 for d = [0.01, 0.05, 0.1, 0.5]
     
-    fprintf('----- %d -------\n', fid)
-    main2(5000, 5, 15, w, 1.0, fid, d);
+    fprintf('----- %f: noise %f -------\n', fid, d)
+    main3(5000, 5, 15, 2.5, 1.0, fid, d);
     
     fid = fid + 0.1;
 end
