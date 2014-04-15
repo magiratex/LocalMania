136d98f16dfaa3947114aabde25f64e651fc67e7
magiratex <magiratexTrash@gmail.com>
2014-04-15 23:14:12 +0800

    update

--- results/check_data.m
+++ results/check_data.m
@@ -1,3 +1,5 @@
+%%
+
 clc
 for i = 10.1: 0.1: 11.3
     fname = sprintf('data_%.1f.mat',i);
@@ -6,4 +8,31 @@ for i = 10.1: 0.1: 11.3
 %     fprintf('%.3f %.4f\n', backup.Gr.w, sqrt(sum(sum((backup.Gr.trans - backup.iter(end-1).T).^2))));
     fprintf('%.4f %.4f\n', sqrt(sum(sum((backup.Gr.trans - backup.iter(end-1).T).^2))), ...
                       sqrt(sum(sum((backup.Gr.trans - backup.hmmT).^2))));
+end;
+
+%%
+
+clc
+for i = 1 : 7
+    fname = sprintf('data_21.%d.mat',i);
+    load(fname);
+%     fprintf('%.3f %.3f %.3f\n', backup.Gr.w, backup.iter(end).w,backup.iter(end-1).w);
+%     fprintf('%.3f %.4f\n', backup.Gr.w, sqrt(sum(sum((backup.Gr.trans - backup.iter(end-1).T).^2))));
+    fprintf('%.4f %.4f\n', sqrt(sum(sum((backup.Gr.trans - backup.iter(end-1).T).^2))), ...
+                      sqrt(sum(sum((backup.Gr.trans - backup.hmmT).^2))));
+end;
+
+%%
+
+clc
+for i = 1 : 4
+    fname = sprintf('data_30.%d.mat',i);
+    load(fname);
+%     fprintf('%.3f %.3f %.3f\n', backup.Gr.w, backup.iter(end).w,backup.iter(end-1).w);
+    fprintf('%.3f %.4f %.4f %.4f\n', backup.Gr.w, ...
+                    sqrt(sum(sum((backup.Gr.trans - backup.iter(end-1).T).^2))), ...
+                    sqrt(sum(sum((backup.Gr.trans - backup.iter(end).T).^2))), ...
+                    sqrt(sum(sum((backup.Gr.T0 - backup.iter(end).T).^2))));
+%     fprintf('%.4f %.4f\n', sqrt(sum(sum((backup.Gr.trans - backup.iter(end-1).T).^2))), ...
+%                       sqrt(sum(sum((backup.Gr.trans - backup.hmmT).^2))));
 end;
