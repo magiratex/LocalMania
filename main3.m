b33a436f3030d8ff4d960cc2e66ffda924c2c647
magicpiratex <magicpiratex@gmail.com>
2014-04-16 00:50:52 +0800

    exp4 data

--- main3.m
+++ main3.m
@@ -29,7 +29,7 @@ Gr = init_graph(sizeG, edgeN, gtW);
 
 
 %% generate sequences
-[seq, hseq, Gr] = generate_data(Gr, nsample, false, d);
+[seq, hseq, Gr] = generate_data(Gr, nsample, true, d);
 
 
 M = size(Gr.G, 1);
@@ -43,13 +43,13 @@ M = size(Gr.G, 1);
 % gplot(Gr.G, coordinates);
 
 MMin = 1e-5;
-beta = 0.5;
+beta = 0.01;
 eHat = [zeros(1, M); 
         diag(ones(1, M))];    
 % eHat = [diag(ones(1, M))];
 % attrPrior = max((Gr.attr + rand(M)*0.1), 0) .* Gr.G;
-attrPrior = (Gr.attr + rand(M)*d) .* Gr.G;
-% attrPrior = Gr.attr;
+% attrPrior = (Gr.attr + rand(M)*d) .* Gr.G;
+attrPrior = Gr.attr;
 % attrPrior = (Gr.attr + 0.1) .* Gr.G;
 
 backup.rngSeed = rseed;
