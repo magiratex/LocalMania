1de3cea076214ac38be3847ed68823574b3f5644
magiratex <magiratexTrash@gmail.com>
2014-04-27 14:34:52 +0800

    estimate the prior

--- est_prior.m
+++ est_prior.m
@@ -1,3 +1,7 @@
-function est_prior(x, xhat)
+function xval = est_prior(x, xhat)
 
-bsxfun(@times, xhat, K)
+f = @(K) sum(sum((bsxfun(@times, x, K)-xhat).^2));
+
+options = optimoptions('fminunc','Display', 'off', 'TolFun', 1e-4);
+[K, fval] = fminunc(f, P, options);
+xval = bsxfun(@times, x, K);
