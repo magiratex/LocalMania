eafe6f3a59c75ab19d0ede07ba39b82f220fc531
magicpiratex <magicpiratex@gmail.com>
2014-05-23 02:23:18 +0800

    update

---
+++ sim/main.m
@@ -0,0 +1,12 @@
+global goalSelectType;
+goalSelectType = 'rnd';
+lazy_sim;
+lazy_sim_sf;
+
+goalSelectType = 'norep';
+lazy_sim;
+lazy_sim_sf;
+
+goalSelectType = 'dcm';
+lazy_sim;
+lazy_sim_sf;
--- sim2/goal_select.m
+++ sim2/goal_select.m
@@ -1,5 +1,7 @@
 function gid = goal_select(G, agt, varargin)
 
+global goalSelectType;
+
 gid = -1;
 
 if ~isempty(varargin)
@@ -13,82 +15,85 @@ if strcmp(agt.state, 'gs')
 %             return;
 %         end;
 %     end;
-    %% random-0 rep
+
+     switch goalSelectType
+        case 'rnd'
+            %% random-0 rep
+
+            isPortal = find(Gr.portals(1,:) == agt.seq(end));
+            if ~isempty(isPortal) && length(agt.seq)>1
+                connected = find(G(agt.gid, :) == 1);
+                connected = [connected, 0];
+                gid = randi(length(connected)); % naive model
+                gid = connected(gid);
+            else
+                connected = find(G(agt.gid, :) == 1);
+                gid = randi(length(connected)); % naive model
+                gid = connected(gid);
+            end;
     
-%     isPortal = find(Gr.portals(1,:) == agt.seq(end));
-%     if ~isempty(isPortal) && length(agt.seq)>1
-%         connected = find(G(agt.gid, :) == 1);
-%         connected = [connected, 0];
-%         gid = randi(length(connected)); % naive model
-%         gid = connected(gid);
-%     else
-%         connected = find(G(agt.gid, :) == 1);
-%         gid = randi(length(connected)); % naive model
-%         gid = connected(gid);
-%     end;
-    
-    
-    %% random-1 no-repi
-    connected = find(G(agt.gid, :) == 1);
-    isPortal = find(Gr.portals(1,:) == agt.seq(end));
-    if ~isempty(isPortal) && length(agt.seq)>1    
-        connected = [connected, 0];
-    end;
-    while 1
-        gid = randi(length(connected)); % naive model
-        gid = connected(gid);
-        if gid ~= agt.seq(end) && length(agt.seq)==1
-            break;
-        end;
-        if length(agt.seq)>1 && gid ~= agt.seq(end-1) && gid ~= agt.seq(end)
-            break;
-        end;
-    end;
-    
-    %% dcm
-%     if length(agt.seq) <= 1
-%         goodstart = find(G(agt.seq, :));
-%         gid = goodstart(randi(length(goodstart)));
-%     else
-%         w = 4.5;
-%         if length(varargin)==2
-%             goalList = varargin{2};
-%             DYNAMIC = 1;
-%         else
-%             DYNAMIC = 0;
-%         end;
-%         hypG = Gr.G;
-%         ind = Gr.hypind;
-%         attr = Gr.attr;
-%         id = find(find_ind(agt.seq(end-1:end), ind) ~= 0);
-%         vind = find(hypG(id, :));
-%         if DYNAMIC % dynamically changing attraction
-%             dw = compute_dynamic_factor(vind, goalList, Gr.hypind);
-%             attrList = dw .* attr(id, vind);
-%             isPortal = find(Gr.portals(1,:) == agt.seq(end));
-%             if ~isempty(isPortal)
-%                 attrList = [attrList, Gr.portals(2,isPortal)];
-%             end;
-%             prob = exp(w * attrList) ./ sum(exp(w * attrList));
-%         else % static attraction
-%             attrList = attr(id, vind);
-%             isPortal = find(Gr.portals(1,:) == agt.seq(end));
-%             if ~isempty(isPortal)
-%                 attrList = [attrList, Gr.portals(2,isPortal)];
-%             end;
-%             prob = exp(w * attrList) ./ sum(exp(w * attrList));
-%         end;
-%         
-%         
-%         selectID = randsample(length(prob), 1, true, prob);
-%         if ~isempty(isPortal) && selectID == length(prob) % choose to leave
-%             gid = 0;
-%             return;
-%         end;
-%         gid = ind(vind(selectID), 2);
-%         if ind(vind(selectID), 1) ~= agt.seq(end)
-%             disp('something wrong!');
-%         end;
-%     end;
-    
+         case 'norep'
+            %% random-1 no-repi
+            connected = find(G(agt.gid, :) == 1);
+            isPortal = find(Gr.portals(1,:) == agt.seq(end));
+            if ~isempty(isPortal) && length(agt.seq)>1    
+                connected = [connected, 0];
+            end;
+            while 1
+                gid = randi(length(connected)); % naive model
+                gid = connected(gid);
+                if gid ~= agt.seq(end) && length(agt.seq)==1
+                    break;
+                end;
+                if length(agt.seq)>1 && gid ~= agt.seq(end-1) && gid ~= agt.seq(end)
+                    break;
+                end;
+            end;
+         case 'dcm'
+            %% dcm
+            if length(agt.seq) <= 1
+                goodstart = find(G(agt.seq, :));
+                gid = goodstart(randi(length(goodstart)));
+            else
+                w = 4.5;
+                if length(varargin)==2
+                    goalList = varargin{2};
+                    DYNAMIC = 1;
+                else
+                    DYNAMIC = 0;
+                end;
+                hypG = Gr.G;
+                ind = Gr.hypind;
+                attr = Gr.attr;
+                id = find(find_ind(agt.seq(end-1:end), ind) ~= 0);
+                vind = find(hypG(id, :));
+                if DYNAMIC % dynamically changing attraction
+                    dw = compute_dynamic_factor(vind, goalList, Gr.hypind);
+                    attrList = dw .* attr(id, vind);
+                    isPortal = find(Gr.portals(1,:) == agt.seq(end));
+                    if ~isempty(isPortal)
+                        attrList = [attrList, Gr.portals(2,isPortal)];
+                    end;
+                    prob = exp(w * attrList) ./ sum(exp(w * attrList));
+                else % static attraction
+                    attrList = attr(id, vind);
+                    isPortal = find(Gr.portals(1,:) == agt.seq(end));
+                    if ~isempty(isPortal)
+                        attrList = [attrList, Gr.portals(2,isPortal)];
+                    end;
+                    prob = exp(w * attrList) ./ sum(exp(w * attrList));
+                end;
+
+
+                selectID = randsample(length(prob), 1, true, prob);
+                if ~isempty(isPortal) && selectID == length(prob) % choose to leave
+                    gid = 0;
+                    return;
+                end;
+                gid = ind(vind(selectID), 2);
+                if ind(vind(selectID), 1) ~= agt.seq(end)
+                    disp('something wrong!');
+                end;
+            end;
+     end;
 end;
