cd816f43e275a5e2bc5cbb65040793ce9c2d5482
magiratex <magiratexTrash@gmail.com>
2014-05-24 21:32:56 +0800

    edited a little on graph info

--- sim2/dispdata.m
+++ sim2/dispdata.m
@@ -2,23 +2,29 @@ clc;
 clear;
 close all;
 
-load data_rvo_dcm_Campus.mat;
+% load data_rvo_dcm_Campus.mat;
 
 fig = imread('map4.png');
 fig2 = imresize(fig, .95);
 fig = fig2;
 
-for t = 1 : 6000
+sc = 0.1;
+load frameInfo_rvo_dcm_CampusK.mat;
+
+for t = 3000 : length(frameInfo)
     imshow(fig);
     hold on;
-    N = t / 10;
     
-    for i = 1 : N+1
-        
-        % check if the agent i is out
-        
+    conf = frameInfo(t).moving / sc;
+    for i = 1 : size(conf, 1)
+        plot(conf(:,3), conf(:,4), 'o');
+    end;
+    stay = frameInfo(t).staying / sc;
+    for i = 1 : size(stay, 1)
+        plot(stay(:,1), stay(:,2), 'or');
     end;
     
+    
     hold off;
-    pause;
+    pause(0.04);
 end;
